1b09b31306e95eaec3e69d51e2aef6831ae66eed
Johannes Beck <mail@johannes-beck.name>
2012-12-28 14:56:33 +0100

    Change directories in run script

--- etc/omx_runner.sh
+++ etc/omx_runner.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 PLAY_FILE=$1
 shift
-OMXPLAYER_OPTIONS=$@;
+OMXPLAYER_OPTIONS=$@
+OMXPLAYER_CURRENT="../data/omxplayer_current.txt"
 FIFO=/tmp/omxplayer_fifo
-( omxplayer $OMXPLAYER_OPTIONS "$PLAY_FILE" < $FIFO ; rm "/tmp/omxplayer_current.txt" ) >/dev/null 2>&1 &
-echo -n  > $FIFO
+( omxplayer $OMXPLAYER_OPTIONS "$PLAY_FILE" < $FIFO ; rm "$OMXPLAYER_CURRENT" ) >/dev/null 2>&1 &
+echo -n > $FIFO
